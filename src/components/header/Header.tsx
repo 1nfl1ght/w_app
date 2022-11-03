728985a66ea5512a599181b266ad5bfe24912289
1nfl1ght <pavelvinogradov2000@mail.ru>
2022-11-03 22:59:33 +0300

    ended info top block; refactored header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Switch } from './Switch';
-
 import s from './Header.module.scss';
+import "../../styles/index.scss";
 
 type Props = {}
 
 export const Header = (props: Props) => {
   return (
-    <div className={s.container}>
+    <div className="container">
       <header className={s.header}>
-        <div id={s.wrapper1}>
-          <div className={s['menu-icon']}></div>
-          <div className={s['notifications-icon']}></div>
-          <div className={s['current-place']}><b>Alushta</b>, Russia</div>
-        </div>
-        <div id={s.wrapper2}>
-          <div className={s['search']}>
-            <input type="search" name="search-city" id="search" placeholder='Search'/>
+        <div className={s.left_container}>
+          <div id={s.wrapper1}>
+            <div className={s['menu-icon']}></div>
+            <div className={s['notifications-icon']}></div>
+            <div className={s['current-place']}><b>Alushta</b>, Russia</div>
+          </div>
+          <div id={s.wrapper2}>
+            <div className={s['search']}>
+              <input type="search" name="search-city" id="search" placeholder='Search'/>
+            </div>
           </div>
         </div>
         <div id={s.wrapper3}>
--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Info_switch } from './Info_switch'
 import { Today } from '../Today/Today'
+import  "../../styles/index.scss"
+import './info.scss'
 
 type Props = {}
 
 export const Info = (props: Props) => {
   return (
-    <div className='info'>
-        <Info_switch></Info_switch>
-        <Today></Today>
+    <div className="container">
+      <div className='info'>
+        <div className="left_container">
+          <div className="into__top_block">
+            <div className="text_block">
+              <p>Today</p>
+              <p>Tomorrow</p>
+              <p style={{opacity: '1' , fontWeight: '700'}}>Next 5 days</p>
+            </div>
+            <div className="switch_block">
+              <Info_switch></Info_switch>
+            </div>
+          </div>
+          <Today></Today>
+        </div>
+      </div>
     </div>
   )
 }
